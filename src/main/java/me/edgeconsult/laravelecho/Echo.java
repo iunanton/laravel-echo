69ea56ebf81a838a89f867d508e1eebae355b271
Anton Iun <iun@edgeconsult.me>
2018-12-31 20:53:18 +0900

    Add constructors

--- src/main/java/me/edgeconsult/laravelecho/Echo.java
+++ src/main/java/me/edgeconsult/laravelecho/Echo.java
@@ -1,7 +1,29 @@
 package me.edgeconsult.laravelecho;
 
+import io.socket.client.IO;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Echo {
+    public Echo(String uri) {
+        this(uri, null);
+    }
+
+    public Echo(String uri, IO.Options opts) {
+        //this(new URI(uri), null);
+    }
+
+    public Echo(URI uri) {
+        this(uri, null);
+    }
+
+    public Echo(URI uri, IO.Options opts) {
+
+    }
+
     public static void main(String args[]) {
-        System.out.println("Echo");
+        System.out.println("Echo.main()");
+        //Echo echo = new Echo("");
     }
 }
